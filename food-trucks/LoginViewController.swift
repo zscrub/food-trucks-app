037c112347e1f65989ca4d62c4f6cb96b7b42ea9
Zack <rubinzack40@gmail.com>
2021-06-22 23:28:13 -0400

    added done button to text fields, adjusted text field sizes

--- food-trucks/LoginViewController.swift
+++ food-trucks/LoginViewController.swift
@@ -8,10 +8,14 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    @IBOutlet weak var login_btn: UIButton!
+    @IBOutlet weak var password_text: UITextField!
+    @IBOutlet weak var username_text: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        
         // Do any additional setup after loading the view.
     }
     
