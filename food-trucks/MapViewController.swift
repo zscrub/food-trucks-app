ca7f0581897b06fc4c6aaadbbd5b3223fb96bcb1
Zack <rubinzack40@gmail.com>
2021-06-27 18:47:37 -0400

    getData method moved to global scope + outside default viewcontroller

---
+++ food-trucks/MapViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MapViewController.swift
+//  food-trucks
+//
+//  Created by Zack on 6/27/21.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    @IBOutlet weak var allText: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        allText.text = "Loading..."
+        
+//        getData(zip: (zipcode.text!))
+//        for text in info {
+//            allText.text += text
+//        }
+        
+        // Do any additional setup after loading the view.
+    }
+
+}
--- food-trucks/ViewController.swift
+++ food-trucks/ViewController.swift
@@ -8,61 +8,13 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var allText: UITextView!
-//    @IBOutlet weak var allText_: UITextView!
     @IBOutlet weak var place: UILabel!
     @IBOutlet weak var zipcode: UITextField!
-//    @IBAction func displayResults(data: [String]) {
-//        allText.text = "\(data[0])\n"
-//    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-//        getData(zip: (zipcode.text!))
-//        displayResults(data: [""])
-//        allText.text = ""
-    }
-    
-    @IBAction func search(_ sender: UIButton) {
-//        allText.text += "Zip code: \(zipcode.text!)\n"
-        getData(zip: (zipcode.text!))
-    }
-    
-    private func getData(zip: String) {
-        if zip != "" {
-            allText.text = "Loading..."
-            let url = "https://us1.locationiq.com/v1/search.php?key=pk.9684621c328d7f6c2548e794b8b05772&postalcode=" + zip + "&country=us&format=json"
-            let task = URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: {data, response, error in
-                guard let data = data, error==nil else {
-                    print("error")
-                    return
-                }
-                var result: [Response]?
-                do {
-                    result = try JSONDecoder().decode([Response].self, from: data)
-                }
-                catch {
-                    print(String(describing: error))
-                }
-                
-                guard let json = result else {
-                    return
-                }
-                
-                print("changing label...")
-                self.allText.text = zip + " " + json[0].display_name + "\n latitude: " + json[0].lat + "\n longitude: " + json[0].lon
-                print("label changed")
-            })
-            task.resume()
-            }
         
-        }
-}
-
-struct Response: Codable {
-    let place_id: String
-    let lat: String
-    let lon: String
-    let boundingbox: Array<String>
-    let display_name: String
+    }
 }
 
