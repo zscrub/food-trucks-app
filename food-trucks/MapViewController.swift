bba6169b324cc68df7321e60de73fea7e4b74f0e
Zack <rubinzack40@gmail.com>
2021-06-29 17:58:20 -0400

    added app icon, mapView has annotation code, needs circle code

--- food-trucks/MapViewController.swift
+++ food-trucks/MapViewController.swift
@@ -8,22 +8,55 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate {
+    @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var allText: UITextView!
     
     var zip_ = ""
     override func viewDidLoad() {
         super.viewDidLoad()
-        allText.text = zip_
-        
-        fetchUser(zip: zip_, zipCompletionHandler: { data, error in
+        self.mapView.delegate = self
+//        allText.text = zip_
+
+        getData(zip: zip_, zipCompletionHandler: { data, error in
           if let data = data {
             print("display_name is \(data[0].display_name)")
-            self.allText.text = "\(data[0].display_name),\n\(data[0].lat)\n\(data[0].lon)"
+            let lat = data[0].lat
+            let lon = data[0].lon
+            if let latDouble = Double(lat) {
+                if let lonDouble = Double(lon) {
+                    let location = CLLocationCoordinate2D(latitude: CLLocationDegrees(latDouble), longitude: CLLocationDegrees(lonDouble))
+//                    let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+//                    let region = MKCoordinateRegion(center: location, span: span)
+//                    print(lat, lon)
+//                    self.mapView.setRegion(region, animated: true)
+//                    let annotation = MKPointAnnotation()
+//                    annotation.coordinate = location
+//                    annotation.title = self.zip_
+//                    annotation.subtitle = data[0].display_name
+//                    self.mapView.addAnnotation(annotation)
+//
+//                    let region = CLCircularRegion(center: location, radius: 15000, identifier: "zipCircle")
+
+                    DispatchQueue.global(qos: .background).async {
+                        DispatchQueue.main.async {
+                            self.mapView.removeOverlays(self.mapView.overlays)
+                            self.mapView.addOverlay(MKCircle(center: location, radius: 15000))
+                        }
+                    }
+                    
+                }
+            }
           }
         })
 
-        // Do any additional setup after loading the view.
     }
-
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if let overlay = overlay as? MKCircle {
+            let circleRenderer = MKCircleRenderer(circle: overlay)
+            circleRenderer.fillColor = UIColor.blue
+            return circleRenderer
+        }
+    return MKOverlayRenderer(overlay: overlay)
+    }
 }
--- food-trucks/ZipCodeViewController.swift
+++ food-trucks/ZipCodeViewController.swift
@@ -20,7 +20,6 @@ class ZipCodeViewController: UIViewController {
 
     }
     
-    
     @IBAction func search(_ sender: UIButton) {
         zip = zipcode.text!
         performSegue(withIdentifier: "zip", sender: self)
@@ -33,7 +32,7 @@ class ZipCodeViewController: UIViewController {
     }
 }
 
-func fetchUser(zip: String, zipCompletionHandler: @escaping ([Response]?, Error?) -> Void) {
+func getData(zip: String, zipCompletionHandler: @escaping ([Response]?, Error?) -> Void) {
     let url = URL(string: "https://us1.locationiq.com/v1/search.php?key=pk.9684621c328d7f6c2548e794b8b05772&postalcode=\(zip)&country=us&format=json")!
     let task = URLSession.shared.dataTask(with: url, completionHandler: { data, response, error in
 
@@ -57,49 +56,6 @@ func fetchUser(zip: String, zipCompletionHandler: @escaping ([Response]?, Error?
   task.resume()
   // function will end here and return
   // then after receiving HTTP response, the completionHandler will be called
-}
-
-
-public func getData(zip: String) -> String {
-    var info = ""
-    var box = [String]()
-    if zip != "" {
-//        allText.text = "Loading..."
-        let url = "https://us1.locationiq.com/v1/search.php?key=pk.9684621c328d7f6c2548e794b8b05772&postalcode=" + zip + "&country=us&format=json"
-        let task = URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: {(data, response, error) in
-            guard let data = data, error==nil else {
-                print("error")
-                return
-            }
-            var result: [Response]?
-            do {
-                result = try JSONDecoder().decode([Response].self, from: data)
-            }
-            catch {
-                print(String(describing: error))
-            }
-            
-            guard let json = result else {
-                return
-            }
-            
-            
-            print("getting data...")
-//            allText.text = zip + " " + json[0].display_name + "\n latitude: " + json[0].lat + "\n longitude: " + json[0].lon
-            print("info before: \(info)")
-            info += "\(json[0].display_name)\n\(json[0].lat)\n\(json[0].lon)"
-            print("info after: \(info)")
-
-            box.append(contentsOf: json[0].boundingbox)
-            print("data recieved")
-            
-//            return (json[0].display_name, zip, json[0].boundingbox)
-            
-        })
-    task.resume()
-    }
-//    print("and finally \(info)")
-return info
 }
 
 struct Response: Codable {
