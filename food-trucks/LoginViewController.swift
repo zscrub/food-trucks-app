74aeca932e4e3b318268db937fbfbf328a0f1e26
Zack <rubinzack40@gmail.com>
2021-07-02 04:11:52 -0400

    set up api call for login route

--- food-trucks/LoginViewController.swift
+++ food-trucks/LoginViewController.swift
@@ -8,25 +8,49 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-    @IBOutlet weak var login_btn: UIButton!
-    @IBOutlet weak var password_text: UITextField!
-    @IBOutlet weak var username_text: UITextField!
+    @IBOutlet weak var login: UITextField!
+    @IBOutlet weak var password_: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
-        
         // Do any additional setup after loading the view.
     }
 
-    /*
-    // MARK: - Navigation
+    
+    @IBAction func login_btn(_ sender: Any) {
+        let login_ = String(describing: login.text!)
+        let password_ = String(describing: password_.text!)
+        let account_type = "c"
+        
+        // ngrok 
+        let url_ = "https://0b7703232450.ngrok.io"
+        
+        let params = ["username":login_, "email":login_, "password":password_, "account_type":account_type]
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        var request = URLRequest(url: URL(string: "\(url_)/users/login")!)
+    //        var request = URLRequest(url: URL(string: "http://127.0.0.1:8080/users/new")!)
+        request.httpMethod = "GET"
+        request.httpBody = try? JSONSerialization.data(withJSONObject: params, options: [])
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        let session = URLSession.shared
+        let task = session.dataTask(with: request, completionHandler: { data, response, error -> Void in
+            guard let data = data else { return }
+            if response != nil {
+                print(response!)
+            }
+            else {
+                print("No response... API down??")
+            }
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data) as! Dictionary<String, AnyObject>
+                print(json)
+            } catch {
+                print("error")
+            }
+            
+        })
+        task.resume()
     }
-    */
-
+    
 }
--- food-trucks/SignUpViewController.swift
+++ food-trucks/SignUpViewController.swift
@@ -11,22 +11,48 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var username: UITextField!
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var accCreatedLabel: UILabel!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
+    @IBAction func createAcc() {
+        let username_ = String(describing: username.text!)
+        let email_ = String(describing: email.text!)
+        let password_ = String(describing: password.text!)
+        let account_type = "c"
+        
+        let url_ = "https://0d0350dc133d.ngrok.io"
+        
+        let params = ["username":username_, "email":email_, "password":password_, "account_type":account_type]
 
-    /*
-    // MARK: - Navigation
+        var request = URLRequest(url: URL(string: "\(url_)/users/new")!)
+//        var request = URLRequest(url: URL(string: "http://127.0.0.1:8080/users/new")!)
+        request.httpMethod = "POST"
+        request.httpBody = try? JSONSerialization.data(withJSONObject: params, options: [])
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let session = URLSession.shared
+        let task = session.dataTask(with: request, completionHandler: { data, response, error -> Void in
+            guard let data = data else { return }
+            if response != nil {
+                print(response!)
+            }
+            else {
+                print("No response... API down??")
+            }
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data) as! Dictionary<String, AnyObject>
+                print(json)
+            } catch {
+                print("error")
+            }
+            
+        })
+        task.resume()
     }
-    */
-
 }
